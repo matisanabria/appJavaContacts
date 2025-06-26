f48c54617a737e8f55d95e09377b2574a59e24c0
matisanabria <sanabria.mati29@gmail.com>
2025-06-25 23:30:04 -0300

    Borro codigo no necesario

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,7 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
-    static ArrayList<Contact> contactos = new ArrayList<>();
-    static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
         new ContactManager().start();
         System.out.println("\n\n✌ Cerrando app contactos."); // Mensaje al terminar programa
-        scanner.close();
-
     }
 
 
