ff58a72bdb7a84f148dbce9d9d6acde7f140ab58
matisanabria <sanabria.mati29@gmail.com>
2025-06-25 23:23:19 -0300

    Agregamos getters que faltaban

--- src/Contact.java
+++ src/Contact.java
@@ -7,6 +7,11 @@ public class Contact {
         this.email = email;
     }
 
+    public String getNombre() { return nombre; }
+    public String getTelefono() { return telefono; }
+    public String getEmail() { return email; }
+
+
     @Override
     public String toString(){ // Usaremos esto para imprimir el contacto
         return nombre + " - " + telefono;
