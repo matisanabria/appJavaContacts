6061e823e961b9518dcad41dc6b0fb94cd70f6c5
matisanabria <sanabria.mati29@gmail.com>
2025-06-25 23:55:47 -0300

    Empezando con el sistema para guardar en JSON

---
+++ src/JsonHandler.java
@@ -0,0 +1,13 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+
+public class JsonHandler {
+
+}
