eb46d48aba546c76b930b92718d2b1c78483a11b
matisanabria <sanabria.mati29@gmail.com>
2025-06-26 00:12:56 -0300

    Borrando SOUT innecesario

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         new ContactManager().start();
-        System.out.println("\n\n✌ Cerrando app contactos."); // Mensaje al terminar programa
     }
 
 
